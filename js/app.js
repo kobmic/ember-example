054e245d3d0829c30f247180be315137fc715821
Michael Kober <michael.kober@jayway.com>
2013-05-21 09:50:01 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,38 @@
+App = Ember.Application.create();
+
+
+App.SearchTextField = Ember.TextField.extend({
+    insertNewline: function(){
+        App.tweetsController.loadTweets();
+    }
+});
+
+App.tweetsController = Ember.ArrayController.create({ content: [], username: '', loadTweets: function () {
+    var me = this;
+    var username = me.get("username");
+
+    if (username) {
+        var url = 'http://api.twitter.com/1/statuses/user_timeline.json'
+        url += '?screen_name=%@&callback=?'.fmt(me.get("username"));
+
+        $.getJSON(url, function (data) {
+            me.set('content', []);
+            $(data).each(function (index, value) {
+                var t = App.Tweet.create({
+                    avatar: value.user.profile_image_url,
+                    screen_name: value.user.screen_name,
+                    text: value.text,
+                    date: value.created_at });
+                me.pushObject(t);
+            })
+        });
+
+    } } });
+
+App.Tweet = Ember.Object.extend({ avatar: null, screen_name: null, text: null, date: null });
+
+App.Router.map(function() {
+    this.route("tweets"); // short for: this.route("tweets", { path: "/tweets" });
+    this.route("about");
+});
+
